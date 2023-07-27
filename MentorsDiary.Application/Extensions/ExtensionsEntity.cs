b1dde0b035f16e2dd47a74bc8e45a93a6d6f57d2
Заид Мингалиев <MingalievZZ@stud.kai.ru>
2023-07-27 10:00:26 +0300

    Fix add students in group event

---
+++ MentorsDiary.Application/Extensions/ExtensionsEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MentorsDiary.Application.Extensions;
+
+/// <summary>
+/// Class ExtensionsEntity.
+/// </summary>
+public static class ExtensionsEntity
+{
+    /// <summary>
+    /// Clears the specified database set.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="dbSet">The database set.</param>
+    public static void Clear<T>(this DbSet<T> dbSet) where T : class
+    {
+        if (dbSet.Any())
+        {
+            dbSet.RemoveRange(dbSet.ToList());
+        }
+    }
+}
