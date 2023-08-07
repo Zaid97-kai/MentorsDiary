b285b8af9ce166fdee0ebb47b4e4ef460e8bca4e
Shvyrkaloff <shvyrkalovm@mail.ru>
2023-08-08 00:57:21 +0300

    Add image for groups

--- MentorsDiary.Web/Components/Groups/GroupItem.razor.cs
+++ MentorsDiary.Web/Components/Groups/GroupItem.razor.cs
@@ -7,6 +7,7 @@ using MentorsDiary.Application.Entities.Groups.Domains;
 using MentorsDiary.Application.Entities.Users.Domains;
 using MentorsDiary.Web.Data.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace MentorsDiary.Web.Components.Groups;
 
@@ -93,6 +94,8 @@ public partial class GroupItem
     /// <value>The divisions.</value>
     private List<Division>? Divisions { get; set; } = new();
 
+    private Application.Entities.Groups.Domains.Group? Clone { get; set; } = new();
+
     /// <summary>
     /// Gets or sets the curators.
     /// </summary>
@@ -110,6 +113,12 @@ public partial class GroupItem
     /// </summary>
     private bool _isLoading;
 
+    private string? _avatar;
+
+    private string? _newAvatar;
+
+    private IBrowserFile? _resizedImage;
+
     #endregion
 
     /// <summary>
@@ -119,6 +128,19 @@ public partial class GroupItem
     protected override async Task OnInitializedAsync()
     {
         await GetListAsync();
+        await UploadAvatarPath();
+    }
+
+    private async Task UploadAvatarPath()
+    {
+        if (_group!.ImagePath != null)
+        {
+            var result = await GroupService?.GetAvatarAsync(_group!.ImagePath)!;
+            if (result != null)
+                _avatar = result.RequestMessage?.RequestUri?.ToString();
+            else
+                await MessageService?.Error("Ошибка фотографии")!;
+        }
     }
 
     /// <summary>
@@ -166,6 +188,7 @@ public partial class GroupItem
 
             if (CurrentUser.Role == EnumRoles.DeputyDirector)
                 _group.DivisionId = CurrentUser.DivisionId;
+            await UploadAvatar();
 
             var response = await GroupService.UpdateAsync(_group);
 
@@ -179,5 +202,54 @@ public partial class GroupItem
         StateHasChanged();
 
         NavigationManager.NavigateTo("/group");
+    }
+
+    private async Task UploadAvatar()
+    {
+        using var content = new MultipartFormDataContent();
+        var fileName = Path.GetRandomFileName();
+
+        content.Add(
+            content: new StreamContent(_resizedImage?.OpenReadStream() ?? Stream.Null),
+            name: "\"files\"",
+            fileName: fileName);
+
+        var response = await GroupService?.UploadAvatarAsync(content)!;
+
+        if (response.IsSuccessStatusCode)
+        {
+            _group!.ImagePath = fileName;
+            Clone!.ImagePath = fileName;
+
+            await MessageService.Success("Upload completed successfully.");
+            var result = await GroupService.GetAvatarAsync(_group!.ImagePath);
+            _avatar = result.RequestMessage?.RequestUri?.ToString();
+        }
+        else
+            await MessageService.Error("Upload failed.");
+    }
+
+    private async Task OnInputFileChange(InputFileChangeEventArgs e)
+    {
+        var imageFile = e.File;
+        if (imageFile.ContentType != "image/jpeg" && imageFile.ContentType != "image/png")
+        {
+            await MessageService.Error("You can only upload JPG/PNG file!");
+        }
+        else
+        {
+            _resizedImage = await imageFile.RequestImageFileAsync("image/png", 500, 500);
+
+            var ms = new MemoryStream();
+            await _resizedImage.OpenReadStream().CopyToAsync(ms);
+            var bytes = ms.ToArray();
+
+            var b64 = Convert.ToBase64String(bytes);
+
+            _newAvatar = "data:image/png;base64," + b64;
+            _avatar = _newAvatar;
+        }
+
+        StateHasChanged();
     }
 }
