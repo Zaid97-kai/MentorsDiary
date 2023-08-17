10b5c96aec2c4667a2a140f855965e985cd57ab6
Shvyrkaloff <shvyrkalovm@mail.ru>
2023-08-17 14:31:39 +0300

    add forms

---
+++ MentorsDiary.Desktop.Admin/Forms/UsersListForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MentorsDiary.Desktop.Admin.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для UsersListForm.xaml
+    /// </summary>
+    public partial class UsersListForm : Window
+    {
+        public UsersListForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
